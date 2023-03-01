62540390af4b14419234d487a708645037e20c22
Bhavishyag2 <bhavishyag2@gmail.com>
2023-03-01 13:11:26 +0530

    Scripts for testing

---
+++ scripts/deployInsuranceFund.ts
@@ -0,0 +1,24 @@
+import { ethers, upgrades } from "hardhat";
+
+async function getImplementation(proxyAddr: string) {
+  const proxyAdmin = await upgrades.admin.getInstance();
+
+  return proxyAdmin.getProxyImplementation(proxyAddr)
+}
+
+async function main(){
+
+ const insurancefund = await ethers.getContractFactory("InsuranceFund");
+ const InsuranceFund = await upgrades.deployProxy(insurancefund, {initializer : "initialize" });
+ await InsuranceFund.deployed();
+
+ const impAddr = getImplementation(InsuranceFund.address);
+ console.log("Implementation address", impAddr);
+ console.log("Proxy address", InsuranceFund.address);
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
