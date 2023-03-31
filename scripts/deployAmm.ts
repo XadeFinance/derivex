9c6d1413b473ad67d17b87e8318ae5f048e1befd
Bhavishyag2 <bhavishyag2@gmail.com>
2023-03-31 18:24:30 +0530

    Added BTC Amm address

--- scripts/deployAmm.ts
+++ scripts/deployAmm.ts
@@ -27,19 +27,23 @@ async function main(){
  const baseAsset = BigNumber.from(500).mul(Default_digits);
 
 
+//  console.log(price);
+//  console.log(ETHprice);
  price=price/ETHprice;
- price=price/100000000;
+//  console.log(price);
+//  price=price/100000000;
+//  console.log(price);
  const weiPrice = ethers.utils.parseEther(price.toString());
 
 //  console.log("Price of BTC/USD", price);
 //  console.log("Price of ETH/USD", ETHprice);
-  const Amm = await upgrades.deployProxy(amm, [baseAsset,baseAsset.mul(weiPrice).div(Default_digits),BigNumber.from(90).mul(Default_digits).div(100), BigNumber.from(3600), deployedAdd.PriceFeedL2Imp, "BTC", "0xA3C957f5119eF3304c69dBB61d878798B3F239D9", BigNumber.from(12).mul(Default_digits).div(10000), BigNumber.from(27).mul(Default_digits).div(100000), BigNumber.from(3).mul(Default_digits).div(1000000)], {initializer : "initialize" });
+  const Amm = await upgrades.deployProxy(amm, [baseAsset,baseAsset.mul(weiPrice).div(Default_digits),BigNumber.from(90).mul(Default_digits).div(100), BigNumber.from(3600), deployedAdd.PriceFeedL2Imp, ethers.utils.formatBytes32String("BTC"), "0xA3C957f5119eF3304c69dBB61d878798B3F239D9", BigNumber.from(12).mul(Default_digits).div(10000), BigNumber.from(27).mul(Default_digits).div(100000), BigNumber.from(3).mul(Default_digits).div(1000000)], {initializer : "initialize" });
  await Amm.deployed();
 
-//  const impAddr = getImplementation(Amm.address);
+ const impAddr = await getImplementation(Amm.address);
 
-//  console.log("Implementation address: ", impAddr);
-//  console.log("Proxy address: ",Amm.address);
+ console.log("Implementation address: ", impAddr);
+  console.log("Proxy address: ",Amm.address);
 
 
 }
